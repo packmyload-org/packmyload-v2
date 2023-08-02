42558f60eb22991798287a67aac887100701f700
Samagent97 <sammccarthy090@gmail.com>
2023-08-03 00:57:48 +0100

    almost done with page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import HeroSection from '@/components/landing/HeroSection'
+import Service from './services/Service'
 function Home() {
   return (
     <div className='sm:mt-[6%] mt-[15%] w-full text-black'>
