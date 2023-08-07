9c375a6f382f0a0359bd71c17d73436ace56fdcf
m@nji <spontaneousmanji@gmail.com>
2023-08-07 16:36:38 +0100

    fix: loader

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Loading() {
   return (
-    <div className='w-full absolute min-h-screen z-[9999px] text-black bg-slate-600'>
+    <div className='w-full absolute min-h-screen z-[9999] text-black bg-slate-600'>
       loading..
     </div>
   )
