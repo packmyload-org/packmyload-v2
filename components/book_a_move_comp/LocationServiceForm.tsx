adb50f5e94f63c6226d1009febb5542a32cbff65
Fiki-josh <fikioluwamayomikun@gmail.com>
2023-08-16 13:41:54 +0100

    chore: location-details-page-setup

---
+++ components/book_a_move_comp/LocationServiceForm.tsx
@@ -0,0 +1,98 @@
+import { useState } from "react";
+import { Buildings, SquareHalf, Elevator, Car } from "@phosphor-icons/react";
+import { serviceFor } from "@/utils/bookingService";
+interface DetailsProps {
+    title: string;
+    location: string | React.ReactNode;
+}
+const LocationServiceForm: React.FC<DetailsProps> = ({ title, location}) => {
+
+    const [activeButton, setActiveButton] = useState<string>('');
+    return (
+        <div className="bg-blue-200 p-6 mb-4 rounded-md shadow-md">
+            <p className="text-2xl">{title}</p><br />
+            <p className="text-lg">{location}</p>
+            <div className="flex items-center mt-10" style={{width: 'fit-content'}}>
+                <div className="bg-slate-50 rounded-full p-2">
+                    <Buildings size={32} color="#5bc7dc" />
+                </div>
+                <div className="ml-2 text-lg">                   
+                    Building Type
+                </div>
+            </div>
+            <select name="category" id="" className='w-full mt-3 p-3 px-0 border border-white rounded-md bg-slate-100 outline-none'>
+               <option value="" hidden>Service For</option>
+               {serviceFor.map(item => <option
+                   value={item.label}
+                   key={item.key}
+                 >
+                   {item.label}
+                </option>)}
+            </select>
+            <div className="flex justify-between mt-3" >
+                <div className="flex items-center">
+                    <div className="bg-slate-50 rounded-full p-2">
+                        <SquareHalf size={32} color="#5bc7dc" />
+                    </div>
+                    <div className="ml-2 text-lg">                   
+                        Apt. Unit
+                    </div>
+                </div>
+                <div className="w-[55%] flex flex-col gap-1 items-start mt-2">
+                    <input
+                    type="text"
+                    id='AptUnit'
+                    placeholder="Apt. Unit"
+                    className="w-full h-10 p-4 text-gray-500 rounded-md outline-none"
+                    required
+                    />
+                </div>
+            </div>
+            <div className="flex justify-between mt-3" >
+                <div className="flex items-center">
+                    <div className="bg-slate-50 rounded-full p-2">
+                        <Elevator size={32} color="#5bc7dc" />
+                    </div>
+                    <div className="ml-2 text-lg">                   
+                        Floors
+                    </div>
+                </div>
+                <div className="w-[55%] flex flex-col gap-1 items-start mt-2">
+                    <input
+                    type="number"
+                    id='floors'
+                    placeholder="#"
+                    className="w-full h-10 p-4 text-gray-500 rounded-md outline-none"
+                    required
+                    />
+                </div>
+            </div>
+            <div className="flex justify-between mt-3" >
+                <div className="flex items-center">
+                    <div className="bg-slate-50 rounded-full p-2">
+                        <Car size={32} color="#5bc7dc" />
+                    </div>
+                    <div className="ml-2 text-lg">                   
+                        Parking
+                    </div>
+                </div>
+                <div className="w-[55%] flex justify-between gap-1 items-start mt-2">
+                    <button
+                            className={`w-[45%] px-4 py-2 rounded-md transition-colors duration-300 ${activeButton === 'yes' ? 'bg-red-500 text-white' : 'bg-blue-500 text-white'}`}
+                            onClick={() => setActiveButton('yes')}
+                        >
+                            Yes
+                        </button>
+                        <button
+                            className={`w-[45%] px-4 py-2 rounded-md transition-colors duration-300 ${activeButton === 'no' ? 'bg-red-500 text-white' : 'bg-blue-500 text-white'}`}
+                            onClick={() => setActiveButton('no')}
+                        >
+                            No
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default LocationServiceForm
