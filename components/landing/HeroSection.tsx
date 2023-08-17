6b08618d86cf2a5ed9edfe53098f3755570ab1f6
m@nji <spontaneousmanji@gmail.com>
2023-08-17 17:05:30 +0100

    chore: updating app

--- components/landing/HeroSection.tsx
+++ components/landing/HeroSection.tsx
@@ -1,6 +1,15 @@
+import Loading from '@/app/loading';
+import { Autocomplete, useJsApiLoader } from '@react-google-maps/api';
 import Image from 'next/image';
 export default function HeroSection() {
-  return (
+  const { isLoaded} = useJsApiLoader({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY ?? '',
+    libraries:['places']
+    })
+    if (!isLoaded) {
+      return <Loading/>
+    }
+    return (
     <section className='w-full h-max pb-4 md:pb-0 md:h-[85dvh] relative flex flex-col justify-center'>
      <div className='gradient h-full w-full z-0 absolute' />
 
@@ -13,13 +22,18 @@ export default function HeroSection() {
           <div className="sm:w-full sm:mt-10 ">
             <form action="" className='border-[#9d9d9d] bg-[#9d9d9d] border-[15px] lg:border-[10px] rounded-lg lg:rounded-full w-full mx-auto lg:w-[70%] flex space-y-4 sm:space-y-0 flex-col md:flex-row input_shadow'>
               <div className='flex bg-gray-100 flex-nowrap py-1 px-3 lg:rounded-l-full w-full lg:w-5/12 justify-between border-r-2'>
-              <input type="text" name="" placeholder='Pickup Address' autoComplete='off' required className='p-2 h-10 focus:outline-0 text-gray-500 ' />
+              <Autocomplete>
+                    
+                  <input type="text" name="" placeholder='Pickup Address' autoComplete='off' required className='p-2 h-10 focus:outline-0 text-gray-500 ' />
+              </Autocomplete>
               <Image src="/images/ic_pickup.svg" 
               className='hover:animate-ping'
               alt="moving company lagos nigeria-student moving pickup indicator" width={20} height={20}/>
               </div>
-              <div className='flex bg-gray-100 flex-nowrap py-1 px-3 w-full lg:w-5/12 justify-between'>
+                <div className='flex bg-gray-100 flex-nowrap py-1 px-3 w-full lg:w-5/12 justify-between'>
+                  <Autocomplete>
               <input type="text" name="" placeholder='Destination Address' autoComplete='off' required className='p-2 h-10 focus:outline-0 text-gray-500 ' />
+                  </Autocomplete>
               <Image src="/images/ic_dropdown.svg"
               className='hover:animate-ping'
               alt="moving company lagos nigeria-student moving pickup indicator" width={20} height={20}/>
