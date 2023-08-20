4e51c74f04eb54f27c67b3498fef4d8eaead09c5
m@nji <spontaneousmanji@gmail.com>
2023-08-20 12:52:07 +0100

    chore: a minor fix

---
+++ app/gallery/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+   <div>
+    <h1 className='text-3xl font-mono font-bold text-center'>This feature is coming soon!</h1>
+    </div>
+  )
+}
+
+export default page
