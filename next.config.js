7466c63b90adbab0bdcd06190645f1bd393b55d6
m@nji <spontaneousmanji@gmail.com>
2023-08-20 13:37:19 +0100

    updating config

--- next.config.js
+++ next.config.js
@@ -1,19 +1,30 @@
-/** @type {import('next').NextConfig} */
+// next.config.js
+
 const nextConfig = {
   images: {
     unoptimized: true,
     domains: ["images.unsplash.com", "res.cloudinary.com"],
   },
   experimental: {
-    esmExternals: "loose", // <-- add this
-    serverComponentsExternalPackages: ["mongoose"], // <-- and this
+    esmExternals: "loose",
+    serverComponentsExternalPackages: ["mongoose"],
   },
   webpack: (config) => {
-    // this will override the experiments
     config.experiments = { ...config.experiments, topLevelAwait: true };
-    // this will just update topLevelAwait property of config.experiments
-    // config.experiments.topLevelAwait = true
     return config;
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "script-src 'self' 'unsafe-inline' https://apis.google.com",
+          },
+        ],
+      },
+    ];
   },
 };
 
