dd94f64b08b22966c4e9e17b8d2c0bb691ea3893
Fiki-josh <fikioluwamayomikun@gmail.com>
2023-08-31 14:20:18 +0100

    chore: navigation Fix

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -2,17 +2,19 @@
 // import { useEffect } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-import Logo from '@/public/images/logo.png'
 import {Phone} from '@phosphor-icons/react'
 import MobileNav from './navBar/mobileNav';
 import TabletsNav from './navBar/tabletsNav';
 import Services from './navBar/Service';
 import { usePathname } from 'next/navigation'
+
+const Logo = "https://res.cloudinary.com/packmyload/image/upload/v1545994681/PML1x.png"
+
 function Nav() {  
   const pathname = usePathname()
   return (
     <>
-    <nav className="flex-between w-full px-10 py-3 bg-white border-b-2 border-[#9d9d9d] fixed top-0 z-30">
+    <nav className="flex-between w-full px-10 py-3 bg-[#2E5F9E] border-b-2 border-[#9d9d9d] fixed top-0 z-30">
     <Link href='/' className='flex gap-2 text-center cursor-pointer'>
     <Image 
       src={ Logo }
@@ -25,21 +27,21 @@ function Nav() {
     
     {/* Desktop Navigation  */}
     <div className="lg:flex hidden no-wrap items-center text-sm md:w-5/6 lg:w-1/2">
-        <div className='text-gray-500 w-full gap-10 flex justify-evenly items-center'>
-        <Link href='/services' className=' w-16 text-gray-500 cursor-pointer'>
+        <div className='text-white w-full gap-10 flex justify-evenly items-center'>
+        <Link href='/services' className=' w-16 text-white cursor-pointer'>
           Services
         </Link>
-        <Link href='/gallery' className=' w-16 text-gray-500 cursor-pointer'>
+        <Link href='/gallery' className=' w-16 text-white cursor-pointer'>
           Gallery
         </Link>
         <Services/>
-        <a href="tel:+2347007225776" className='text-gray-500 flex-between w-16 cursor-pointer'>
-        <Phone size={16} color='#667280' weight="fill" />
+        <a href="tel:+2347007225776" className='text-white flex-between w-16 cursor-pointer'>
+        <Phone size={16} color='white' weight="fill" />
           call us
         </a> 
         {
           !pathname.includes("book_a_move") ?
-          <Link href='/book_a_move'  className='flex justify-evenly items-center hover:animate-pulse w-[160px] p-[8px] border-inherit/20 border-[10px] bg-blue-100 hover:bg-blue-600 hover:text-gray-100 text-gray-500 font-bold rounded-full text-xs'>
+          <Link href='/book_a_move'  className='flex justify-evenly items-center hover:animate-pulse w-[160px] p-[8px] border-inherit/20 border-[10px] bg-blue-100 hover:bg-blue-600 hover:text-gray-100 text-white font-bold rounded-full text-xs'>
             BOOK NOW 
             <span className='font-extrabold text-lg max-h-5 text-center flex justify-center items-center'>
               + 
