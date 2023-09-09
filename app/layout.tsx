43ea09df42fecd99e14b804da021bf53be6f160d
m@nji <spontaneousmanji@gmail.com>
2023-09-09 11:39:17 +0100

    chore: implement tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+'use client'
 import dynamic from 'next/dynamic'
 const Nav = dynamic(() => import('@/components/Nav'), {
     loading: ()=> <Loading/>
@@ -20,17 +21,32 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <meta name="description" content="packmyload.com offers is one of the best packing and moving companies in Lagos, Nigeria. We offer international and interstate packing and moving to Lagos. Our staff help you with a swift, fast &amp; efficient move.
-"></meta>
-<meta property="og:image" content="https://www.packmyload.com/images/iconpackmyload.png"/>
-<meta property="og:image:type" content="image/png"></meta>
-<meta property="og:image:height" content="342"></meta>
-<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"></meta>
-<meta property="og:title" content="packmyload.com"/>
-      <link rel="icon" href="/favicon.ico"  />
-      <title>Packmyload</title>
+      <head>
+        {/* <!-- Google Tag Manager --> */}
+        <script
+          dangerouslySetInnerHTML={{__html: `function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P4998J87'`}}
+        />
+        {/* <!-- End Google Tag Manager --> */}
+          <meta name="description" content="packmyload.com offers is one of the best packing and moving companies in Lagos, Nigeria. We offer international and interstate packing and moving to Lagos. Our staff help you with a swift, fast &amp; efficient move.
+    "></meta>
+            <meta property="og:image" content="https://www.packmyload.com/images/iconpackmyload.png"/>
+            <meta property="og:image:type" content="image/png"></meta>
+            <meta property="og:image:height" content="342"></meta>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"></meta>
+            <meta property="og:title" content="packmyload.com"/>
+          <link rel="icon" href="/favicon.ico"  />
+          <title>Packmyload</title>
+      </head>
 
       <body className='font-satoshi'>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P4998J87"
+          height="0" width="0"
+          style={{ display: "none", visibility: "hidden" }}>
+        </iframe> 
         <div className='w-full'>
           <main className='w-full overflow-y-hidden'>
               <Provider>
