eb390473bb1a7b879f042a101fac484d1663eb40
m@nji <spontaneousmanji@gmail.com>
2023-09-12 23:07:09 +0100

    minor fixes

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link"
 import Image from "next/image"
-import NotFoundImage from "@/public/images/iconpackmyload.png"
+import NotFoundImage from "@/public/images/clouds.png"
 
 export default function NotFound() {
   return (
