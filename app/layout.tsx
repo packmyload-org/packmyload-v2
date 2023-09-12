1182b4096aa4423371760e666c1fcdfc71a706a8
m@nji <spontaneousmanji@gmail.com>
2023-09-12 08:57:45 +0100

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-P4998J87');`}}
+})(window,document,'script','dataLayer','GTM-M8S8P8C8')`}}
         />
         {/* <!-- End Google Tag Manager --> */}
         <script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script>
@@ -42,10 +42,9 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       </head>
 
       <body className='font-satoshi'>
-        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P4998J87"
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M8S8P8C8"
           height="0" width="0"
-        >
-        </iframe> 
+        ></iframe>
           <div className='w-full'>
             <main className='w-full overflow-y-hidden'>
                 <Provider>
