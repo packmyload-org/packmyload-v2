6877348a25501725bd33adb7a2d06a1454b34246
Fiki-josh <fikioluwamayomikun@gmail.com>
2023-09-19 22:49:43 +0100

    chore: get started button animation done on each service page and performance optimization done on storge service page

--- app/services/storage/page.tsx
+++ app/services/storage/page.tsx
@@ -1,17 +1,7 @@
-"use client"
 import Image from 'next/image'
-import { 
-    RocketLaunch,
-    WifiHigh,
-    Motorcycle,
-    Cloud,
-    ShoppingCart,
-    PawPrint,
-    TreeEvergreen,
-    Wheelchair
-} from '@phosphor-icons/react';
 import { ServicesHeader } from '@/components/services/ServicesHeader';
 import { MovingForm } from '@/components/services/MovingForm';
+import { IconsSection } from './IconsSection';
 
 export default function Storage(){
 
@@ -70,44 +60,9 @@ export default function Storage(){
                     <section className='mt-3'>
                         <h1 className='text-xl font-bold text-[#566985]'>What We Offer</h1>
                         <br />
-                        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 ">
-                            <section className='flex items-start gap-2'>
-                                <RocketLaunch size={25} color="#4db7fe" weight="fill" />
-                                <span className='text-[#878C9F] mt-[3px] font-medium'>200+ units- different sizes</span>
-                            </section>
-                            <section className='flex items-start gap-2'>
-                                <WifiHigh size={25} color="#4db7fe" />
-                                <span className='text-[#878C9F] mt-[3px] font-medium'>facilities in Lagos</span>
-                            </section>
-                            <section className='flex items-start gap-2'>
-                                <Motorcycle size={28} color="#4db7fe" weight="fill" />
-                                <span className='text-[#878C9F] mt-[3px] font-medium'>Easy Access</span>
-                            </section>
-                            <section className='flex items-start gap-2'>
-                                <Cloud size={25} color="#4db7fe" weight="fill" />
-                                <span className='text-[#878C9F] mt-[3px] font-medium'>Modern, clean facilities</span>
-                            </section>
-                            <section className='flex items-start gap-2'>
-                                <ShoppingCart size={25} color="#4db7fe" weight="fill" />
-                                <span className='text-[#878C9F] mt-[3px] font-medium'>No minimum stay</span>
-                            </section>
-                            <section className='flex items-start gap-2'>
-                                <PawPrint size={25} color="#4db7fe" weight="fill" />   
-                                <span className='text-[#878C9F] mt-[3px] font-medium'>Long term discounts</span>
-                            </section>
-                            <section className='flex items-start gap-2'>
-                                <TreeEvergreen size={25} color="#4db7fe" weight="fill" />   
-                                <span className='text-[#878C9F] mt-[3px] font-medium'>Containers, warehousing</span>
-                            </section>
-                            <section className='flex items-start gap-2'>
-                                <Wheelchair size={25} color="#4db7fe" weight="fill" />  
-                                <span className='text-[#878C9F] mt-[3px] font-medium'>Archiving</span>
-                            </section>
-                            <section className='flex items-start gap-2'>
-                                <Wheelchair size={25} color="#4db7fe" weight="fill" />  
-                                <span className='text-[#878C9F] mt-[3px] font-medium'>Daily/weekly Storage</span>
-                            </section>
-                        </div>
+                        
+                        <IconsSection />
+                        
                     </section>
                 </div>
                 <div className='md:w-[30%] sm:w-[90%]'> 
