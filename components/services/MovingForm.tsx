946e48e11ee775980c48719aea0d9fbcd5db7272
m@nji <spontaneousmanji@gmail.com>
2023-09-21 22:08:32 +0100

    chore: implementing hero form fix

--- components/services/MovingForm.tsx
+++ components/services/MovingForm.tsx
@@ -5,6 +5,7 @@ import { useRouter } from 'next/navigation';
 import { FormEvent } from 'react';
 import { useBookingForm } from '@/context/BookingFormContext';
 import { alerts } from '../alerts/Alert';
+import { DatePicker } from 'antd';
 
 export const MovingForm: React.FC = () => {
     const router = useRouter()
@@ -22,8 +23,8 @@ export const MovingForm: React.FC = () => {
     }
     return (
         <div className="w-5/6 pt-4 sm:mt-14">
-            <form onSubmit={handleSubmit} className='border-white/20 bg-white/20 border-[15px] lg:border-[10px] rounded-lg lg:rounded-full w-full mx-auto lg:w-[800px] flex space-y-4 sm:space-y-0 flex-col md:flex-row input_shadow'>
-              <div className='flex bg-gray-100 flex-nowrap py-1 px-3 lg:rounded-l-full w-full lg:w-5/12 justify-between border-r-2'>
+            <form onSubmit={handleSubmit} className='border-white/20 bg-white/20 border-[15px] lg:border-[10px] rounded-lg lg:rounded-full w-full mx-auto lg:w-[950px] flex space-y-4 sm:space-y-0 flex-col md:flex-row input_shadow'>
+              <div className='flex bg-gray-100 flex-nowrap py-1 px-3 lg:rounded-l-full w-full lg:w-[30%] justify-between border-r-2'>
                   <AutoCompleteInput
                     inputName='pickUp'
                     inputStyle='p-2 h-10 focus:outline-0 text-gray-900 placeholder-blue-300 w-full place_font'
@@ -34,7 +35,7 @@ export const MovingForm: React.FC = () => {
               className='hover:animate-ping object-contain'
               alt="moving company lagos nigeria-student moving pickup indicator" width={20} height={10}/>
               </div>
-                <div className='flex bg-gray-100 flex-nowrap py-1 px-3 w-full lg:w-5/12 justify-between'>
+                <div className='flex bg-gray-100 flex-nowrap py-1 px-3 w-full lg:w-[30%] justify-between border-r-2'>
                    <AutoCompleteInput
                     inputName='destination'
                     inputStyle='p-2 h-10 focus:outline-0 text-gray-900 placeholder-blue-300 w-full place_font'
@@ -45,8 +46,17 @@ export const MovingForm: React.FC = () => {
               className='hover:animate-ping object-contain'
               alt="moving company lagos nigeria-student moving destination indicator" width={20} height={10}/>
               </div>
+                <div className='flex bg-gray-100 flex-nowrap py-1 px-3 w-full lg:w-[30%] justify-between'>
+            <DatePicker
+              className='w-full outline-none'
+              suffixIcon={'x'}
+              bordered={false}
+              picker='week'
+              renderExtraFooter={() => 'extra footer'}
+            />
+              </div>
               <button type="submit" 
-                className='bg-blue-100 hover:bg-white p-3 w-full lg:w-[180px] md:rounded-lg lg:rounded-r-full md:border-[#9d9d9d]/20  text-gray-100 hover:text-blue-300 md:border-8 lg:border-none uppercase text-xs font-bold'>
+                className='bg-blue-100 hover:bg-white p-3 w-full lg:w-[160px] md:rounded-lg lg:rounded-r-full md:border-[#9d9d9d]/20  text-gray-100 hover:text-blue-300 md:border-8 lg:border-none uppercase text-xs font-bold'>
                 Get Quote
               </button>
             </form>
