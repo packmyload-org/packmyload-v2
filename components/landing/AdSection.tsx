34b062a0af2e4183742987196930e10e5ccac545
Fiki-josh <fikioluwamayomikun@gmail.com>
2023-09-30 15:19:48 +0100

    chore: ipad ad-section fix

--- components/landing/AdSection.tsx
+++ components/landing/AdSection.tsx
@@ -8,11 +8,11 @@ const router =useRouter()
             className="bg-[#2E5F9E] w-[100%]  bg-[url('/images/bg/shapes-big-new.png')] "
         >
             <div 
-                className={`max-w-6xl mx-auto flex flex-col py-4 px-7 md:flex-row justify-between`}
+                className={`max-w-6xl mx-auto flex flex-col py-4 px-7 lg:flex-row justify-center lg:justify-between `}
             >
 
-                <section className="w-[100%] md:w-[60%] mt-0  text-center md:text-left md:my-auto">
-                    <span className=" w-[fit-content] mx-auto md:mx-0 flex gap-3 text-white text-2xl md:text-3xl font-bold items-center">
+                <section className="w-[100%] lg:w-[60%] mt-0 text-center lg:text-left lg:my-auto">
+                    <span className="w-full lg:w-[fit-content] flex justify-center gap-3 text-white text-2xl md:text-3xl font-bold items-center">
                         <h1 className="border-r border-white/[.55] pr-2 h-8 ">Packmyload</h1> Hub
                     </span>
 
@@ -22,11 +22,11 @@ const router =useRouter()
                     <p className="text-white text-base mt-2 mx-auto md:mx-0">
                         One concierge service to connect utilities and home services after a move
                     </p>
-                    <button className="mt-8 flex justify-evenly items-center w-[150px] p-[8px] book-move bg-blue-100 hover:bg-white hover:text-blue-300 text-white font-black rounded-full text-base" onClick={()=>router.push('/book_a_move')}>
+                    <button className="mt-8 flex justify-evenly items-center w-[150px] p-[8px] book-move bg-blue-100 hover:bg-white hover:text-blue-300 text-white font-black rounded-full text-base mx-auto lg:mx-0" onClick={()=>router.push('/book_a_move')}>
                         Get Estimate
                     </button>
                 </section>
-                <section className='flex flex-col items-center mt-5 md:mt-0 md:my-auto'>
+                <section className='flex flex-col items-center mt-5 lg:mt-0 lg:my-auto'>
                     <Image 
                         src={"/images/packman.png"}
                         width={345}
