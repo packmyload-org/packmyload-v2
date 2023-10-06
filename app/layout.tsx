80bd01edc70c1d58fd603cbe3072b60d681d149b
m@nji <spontaneousmanji@gmail.com>
2023-10-06 08:12:45 +0100

    chore: minor fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,7 +48,28 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                 {children}
               </Provider>
             </main>
-            <Chat />
+            {/* <Chat /> */}
+            <Script
+            type="text/javascript"
+            strategy="lazyOnload"
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(d, t) {
+                    var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
+                    v.onload = function() {
+                      window.voiceflow.chat.load({
+                        verify: { projectID: '6501924b5212a400086a2372' },
+                        url: 'https://general-runtime.voiceflow.com',
+                        versionID: 'production'
+                      });
+                    }
+                    v.src = "https://cdn.voiceflow.com/widget/bundle.mjs";
+                    v.type = "text/javascript";
+                    s.parentNode.insertBefore(v, s);
+                })(document, 'script');
+              `,
+            }}
+          />
         </div>
       </body>
         
