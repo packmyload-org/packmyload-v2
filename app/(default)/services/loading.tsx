f5972a24108d9b7a6969274d6fcd3e37fc72f12d
m@nji <spontaneousmanji@gmail.com>
2023-10-06 11:09:16 +0100

    chore: updating app

--- app/(default)/services/loading.tsx
+++ app/(default)/services/loading.tsx
@@ -1,9 +1,16 @@
 'use client'
-import Image from 'next/image'
+import {MapPin} from '@phosphor-icons/react'
+
 export default function Loading() {
   return (
-    <div className="w-full absolute top-0 min-h-full z-[9999] bg-slate-600 text-center flex justify-center items-center">
-     <Image src='/images/marker.png' alt='loader' width={100} height={100} className='animate-bounce' priority />
+    <div className="w-full absolute top-0 h-screen z-[9999] bg-slate-600 text-center flex justify-center items-center">
+      <MapPin
+        color="#4bcbfe"
+        size={38}
+        weight='fill'
+        className='animate-bounce'
+      />
+      
     </div>
   );
 }
