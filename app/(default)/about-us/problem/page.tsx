74fe84ae6accb2dacc157221fddfcde0b969ca70
Samagent97 <sammccarthy090@gmail.com>
2023-10-11 08:12:24 +0100

    working on other pages

---
+++ app/(default)/about-us/problem/page.tsx
@@ -0,0 +1,90 @@
+import { Col, Row, Card } from "antd";
+import React from "react";
+
+
+export default function Newsroom() {
+  return (
+    <div className="flex justify-left min-h-screen bg-gray-100 mt-1">
+      <div className="w-full text-black" style={{ marginTop: "24px" }}>
+        <section className="gradient-bg grid items-center py-[20px] min-h-[250px] ">
+          <div className="cirle-bg">
+            <div className="bg object-cover"></div>
+          </div>
+          <div className="container py-12">
+            <div className="join-wrap fl-wrap text-white">
+              <Row
+                className="w-full flex"
+                justify="space-between"
+                align="middle"
+              >
+                <Col span={24} md={10} className="flex flex-col space-y-6">
+                  <h3 className="text-4xl font-bold">
+                    About Packmyload
+                  </h3>
+                  <p >
+                  Read more about our story and history to learn why we're Africa's Most Trusted Moving Company
+                  </p>
+                </Col>
+                <Col
+                  span={24}
+                  md={10}
+                  className="flex items-center text-start md:text-end justify-center min-h-[160px]"
+                ></Col>
+              </Row>
+            </div>
+          </div>
+        </section>
+
+        <div className="w-full bg-[#FFFFFF] h-auto ">
+          <div className="absolute w-full top-[200px] md:top-[285px] lg:top-[300px] p-2"></div>
+          <div>
+            <div className="flex flex-col gap-5 ml-6 lg:flex-row justify-between pt-64 max-w-6xl mx-auto md:pt-[330px] lg:pt-96">
+              <img
+                src="/public/images/about/packmyload_1696572786359.webp"
+                alt="image-1"
+                loading="lazy"
+                className="w-[90%] h-auto md:w-[70%] md:h-auto lg:w-[670px] lg:h-[550px] mx-auto lg:mx-0 rounded-md object-cover "
+              />
+
+              <div className="lg:pt-40 w-[90%] md:w-[70%] lg:w-auto mx-auto lg:mx-0">
+                <p className="text-2xl lg:text-4xl mt-4 text-[#2C353F]">
+                The Stress of Moving
+
+                </p>
+
+                <p className="text-base  ">
+                Why is moving so stressful? <br/>  <br/>
+
+Moving can be a whirlwind of change, bringing both excitement and  <br/>challenges. Even local moves involve finding a new home, adapting to a  <br/>different neighborhood, navigating new commutes, and supporting your  <br/>children as they transition to new schools. Interstate moves add another layer  <br/> of complexity, as you may have to hunt for real estate from a distance, secure <br/> or change jobs, and tackle the intricacies of updating insurance policies, bank <br/> accounts, car registrations, and more.  <br/>  <br/>  <br/>
+
+But amidst all this change, there's one constant: the logistics of moving. <br/> Packing up an entire home or apartment, transporting your belongings, and <br/> unpacking at your new destination can be quite the undertaking, regardless of  <br/>the distance. At Great Guys Moving, we understand these challenges and are <br/> committed to easing your stress by connecting you with top-notch moving  <br/>companies that will help make your relocation a smooth and hassle-free <br/> experience.
+                </p>
+              </div>
+            </div>
+
+            <div className="flex flex-col mr-6 lg:flex-row mt-4 justify-between gap-5 md:shrink-0 max-w-6xl mx-auto">
+              <div className="lg:pt-40 w-[90%] md:w-[70%] lg:w-auto mx-auto lg:mx-0">
+                <p className="text-2xl lg:text-4xl  mt-4 ">
+                  Hiring A Moving Company
+                </p>
+
+                <p className="text-base ">
+                Choosing a professional moving company over a do-it-yourself approach not<br/> only alleviates the physical strain of relocating, but also ensures the safe and<br/> efficient handling of your cherished possessions. However, the difference<br/> between a seamless move and a disastrous one often lies in selecting a<br/> reputable and trustworthy moving company. By partnering with a reliable<br/> mover, you can avoid the pitfalls associated with shady operators and enjoy a<br/> stress-free, expertly managed relocation experience.<br/> <br/>
+
+But, how do you make sure you hire one of these reliable moving companies?<br/> In 2019, Packmyload embarked on a mission to identify and rank the <br/>top moving companies in cities across Nigeria. Our objective was clear: to <br/>help customers connect with the most reliable and trustworthy moving <br/>companies in their area, ensuring these outstanding movers receive the<br/> recognition and clientele they deserve. Unfortunately, many of the popular <br/>consumer platforms being used to research movers offer higher placement to <br/>companies willing to pay a premium. At Packmyload, we believe in an <br/>unbiased approach – you can't purchase your way to the top of our list. Our<br/> commitment is to provide objective, data-driven rankings of the best moving <br/>companies, ensuring you have a smooth and stress-free moving day <br/>experience.
+                </p>
+              </div>
+              <img
+                src="/public/images/about/packmyload_1696572786359.webp"
+                alt="image-2"
+                loading="lazy"
+                className="w-[95%] h-auto md:w-[75%] md:h-auto lg:w-[670px] lg:h-[550px] mx-auto lg:mx-0 rounded-md object-cover pl-5"
+              />
+            </div>
+          </div>
+        </div>
+
+        </div>
+        </div>
+  )
+};
