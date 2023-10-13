68298eec8c89e6b3cfd68da8f957e7e6f58ef727
Fiki-josh <fikioluwamayomikun@gmail.com>
2023-10-13 17:46:23 +0100

    chore: change

--- components/landing/Reviews.tsx
+++ components/landing/Reviews.tsx
@@ -4,7 +4,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 
-import { Pagination, Autoplay } from 'swiper/modules';
+import {  Autoplay } from 'swiper/modules';
 
 export const Reviews: React.FC = () => {
     
