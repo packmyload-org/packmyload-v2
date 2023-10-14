52503501e82779026182c5960907d245863738ed
Fiki-josh <fikioluwamayomikun@gmail.com>
2023-10-14 04:48:30 +0100

    chore: chats component fixes done

--- components/chat/chat.tsx
+++ components/chat/chat.tsx
@@ -1,37 +1,49 @@
 'use client'
-import Image from 'next/image'
 import { useState } from 'react'
 import ChatComponent from './chatComponent'
 import { Chats } from '@phosphor-icons/react'
+import {usePathname} from 'next/navigation'
+
 export default function Chat() {
+  const pathname = usePathname()
+
   const [launch,setLaunch]= useState(true)
   const [start,setStart]= useState(false)
-  return (
-    <>
-        {start ? <ChatComponent setLaunch={setLaunch} launch={launch} start={start} setStart={setStart}/> : (
-    <div className="fixed bottom-2 right-4">
-       <div className='relative border-[10px] border-blue-300 rounded-full p-2 flex flex-col items-center cursor-pointer' onClick={() => { setStart(true); setLaunch(false); }}>
-        {/* <Image
-          src='/images/icons/chats.svg'
-          alt='chats icon'
-          width={40}
-          height={40}
-          className="object-contain"
-        /> */}
-        <Chats size={52} weight="fill" color='#2E5F9E' />
-      {launch &&
-        <div className='absolute bg-white/20 w-[280px] right-full top-[-20%] mr-4'>
-            <div className='w-full flex justify-end'>
-              <p className='font-black text-blue-300 w-max text-lg cursor-pointer' onClick={()=>setLaunch(false)}>x</p>
-            </div>
-            <h1 className='text-base font-medium text-gray-800 w-max mx-2'>Hello there, My name is Mr.Packer,</h1>
-            <h1 className='text-base font-medium text-gray-800 w-max  mx-2'>How can i assist you ?</h1>
+
+  const renderChatBot = pathname.startsWith('/move')
+
+  if(renderChatBot){
+    return null
+  }
+  else {
+
+    return (
+      <>
+          {start ? <ChatComponent setLaunch={setLaunch} launch={launch} start={start} setStart={setStart}/> : (
+      <div className="fixed bottom-2 right-4 z-30">
+         <div className='relative border-[10px] border-blue-300 rounded-full p-2 flex flex-col items-center cursor-pointer' onClick={() => { setStart(true); setLaunch(false); }}>
+          {/* <Image
+            src='/images/icons/chats.svg'
+            alt='chats icon'
+            width={40}
+            height={40}
+            className="object-contain"
+          /> */}
+          <Chats size={46} weight="fill" color='#2E5F9E' />
+        {launch &&
+          <div className='absolute bg-white shadow w-[280px] right-full top-[-20%] mr-4 rounded-md p-1'>
+              <div className='w-full flex justify-end'>
+                <p className='font-black text-blue-300 w-max text-lg cursor-pointer' onClick={()=>setLaunch(false)}>&#10006;</p>
+              </div>
+              <h1 className='text-base font-medium text-gray-800 w-max mx-2'>Hello there, My name is Mr.Packer,</h1>
+              <h1 className='text-base font-medium text-gray-800 w-max  mx-2'>How can i assist you ?</h1>
+          </div>
+       }
         </div>
-     }
-      </div>
-  </div>
-    )}
-   
-   </>
- )
+    </div>
+      )}
+     
+     </>
+   )
+  }
 }
