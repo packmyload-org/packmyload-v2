b3daf24e4d4afbc1c9e7148f250a9394e14d8254
Samagent97 <sammccarthy090@gmail.com>
2023-10-18 01:04:29 +0100

    updating

--- app/(default)/partner/movers/page.tsx
+++ app/(default)/partner/movers/page.tsx
@@ -1,9 +1,9 @@
 import { Col, Row, Card } from "antd";
 
 import React from "react";
-import Image from 'next/image'
+import Image from "next/image";
 
-export default function OurStory() {
+export default function Movers() {
   return (
     <div className="flex justify-left min-h-screen bg-gray-100 mt-1">
       <div className="w-full text-black" style={{ marginTop: "24px" }}>
@@ -19,12 +19,15 @@ export default function OurStory() {
                 align="middle"
               >
                 <Col span={24} md={10} className="flex flex-col space-y-6">
-                  <h3 className="text-4xl font-bold">About Packmyload</h3>
-                  <p>
-                    Read more about our story and history to learn why we're
-                    Africa's Most Trusted Moving Company
-                  </p>
+                  <h3 className="text-4xl font-bold">
+                    Join Our Community of Expert Movers
+                  </h3>
+                  <h2 className=" text-lg mt-2">
+                    Earn a stable income while working in your own time and
+                    space.
+                  </h2>
                 </Col>
+
                 <Col
                   span={24}
                   md={10}
@@ -34,23 +37,40 @@ export default function OurStory() {
             </div>
           </div>
         </section>
-        <div className="w-full bg-[#012D40] h-auto py-20">
+        <div className="w-full  h-auto py-20">
+          <div className="flex flex-row  ">
+            <img
+              src="/images/partner/packmyload_1697583737398.webp"
+              alt="image-1"
+              className="w-[50%] max-h-96   relative bottom-20"
+            />
+            <img
+              src="/images/partner/packmyload_1695798612527.webp"
+              alt="image-2"
+              className="w-[50%] max-h-96   relative bottom-20"
+            />
+            <img
+              src="/images/partner/packmyload_1697583683638.webp"
+              alt="image-3"
+              className="w-[50%] max-h-96   relative bottom-20"
+            />
+          </div>
           <div className="flex flex-col gap-4">
             <div className="flex flex-col gap-5 ml-6 lg:flex-row justify-between  max-w-6xl mx-auto">
               <div className="w-10/12 mx-auto lg:w-full h-full flex items-center">
                 <Image
-                  src="/images/about/packmyload_1696572786359.webp"
+                  src="/images/partner/packmyload_1695798593791.webp"
                   alt="image-1"
                   loading="lazy"
                   className="w-auto h-auto mx-auto lg:mx-0 rounded-md object-cover pr-5 flex-grow"
                   width={600}
                   height={600}
-                  />
+                />
               </div>
 
               <div className=" w-[90%] flex flex-col justify-center ">
-                <p className="text-2xl w-full font-semibold md:w-8/12 lg:text-4xl mt-4 text-white mx-auto">
-                  Who we are
+                <p className="text-2xl w-full font-semibold md:w-8/12 lg:text-4xl mt-4  mx-auto">
+                Enjoy the flexibility of working independently, on your own schedule.
                 </p>
 
                 <p className="text-sm mt-2 w-full md:w-8/12 mx-auto text-white">
@@ -87,15 +107,15 @@ export default function OurStory() {
                 </p>
               </div>
               <div className="w-10/12 mx-auto lg:w-full h-full flex items-center">
-              <Image
-                src="/images/about/packmyload_1696572694895.webp"
-                alt="image-2"
-                loading="lazy"
+                <Image
+                  src="/images/about/packmyload_1696572694895.webp"
+                  alt="image-2"
+                  loading="lazy"
                   className="w-auto h-auto mx-auto lg:mx-0 rounded-md object-cover pl-5 flex-grow"
                   width={600}
                   height={600}
-              />
-              </div >
+                />
+              </div>
             </div>
           </div>
         </div>
