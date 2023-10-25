b75c33d9dd31238efe97b29a8f4acb798364351b
Fiki-josh <fikioluwamayomikun@gmail.com>
2023-10-25 11:44:30 +0100

    chore: ourstory page fix done

--- app/(default)/about-us/ourstory/page.tsx
+++ app/(default)/about-us/ourstory/page.tsx
@@ -5,13 +5,13 @@ import Image from 'next/image'
 
 export default function OurStory() {
   return (
-    <div className="flex justify-left min-h-screen bg-gray-100 mt-1">
+    <div className="flex justify-left min-h-screen bg-gray-100">
       <div className="w-full text-black" style={{ marginTop: "70px" }}>
-        <section className="gradient-bg flex flex-col items-center py-[20px] h-[fit-content] ">
+        <section className="gradient-bg flex flex-col items-center py-[20px] h-[fit-content]">
           <div className="cirle-bg">
             <div className="bg object-cover"></div>
           </div>
-          <div className="container py-12">
+          <div className="container py-12 max-w-6xl">
             <div className="join-wrap fl-wrap text-white">
               <Row
                 className="w-full flex items-center"
@@ -29,7 +29,7 @@ export default function OurStory() {
         </section>
         <div className="w-full bg-[#012D40] h-auto py-20">
           <div className="flex flex-col gap-4">
-            <div className="flex flex-col gap-5 lg:flex-row justify-between max-w-6xl mx-auto">
+            <div className="flex flex-col gap-5 md:flex-row justify-between max-w-6xl mx-auto">
               <div className=" mx-auto w-full h-full">
                 <img
                   src="/images/about/packmyload_1696572786359.webp"
@@ -60,8 +60,8 @@ export default function OurStory() {
               </div>
             </div>
 
-            <div className="flex flex-col lg:flex-row mt-4 pb-8 justify-between gap-5 md:shrink-0 max-w-6xl mx-auto">
-              <div className="lg:pt-40 w-[100%] md:w-[90%] px-2 mx-auto lg:mx-0">
+            <div className="flex flex-col md:flex-row mt-4 justify-between gap-5 md:shrink-0 max-w-6xl mx-auto">
+              <div className="flex flex-col md:justify-center w-[100%] md:w-[90%] px-2 mx-auto lg:mx-0">
                 <p className="text-2xl font-semibold lg:text-4xl  mt-4 text-white">
                   How it works
                 </p>
