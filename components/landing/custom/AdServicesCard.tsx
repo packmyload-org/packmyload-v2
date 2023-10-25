b978db687b9c725734af86efa07c4d0053de6310
Fiki-josh <fikioluwamayomikun@gmail.com>
2023-10-25 16:07:24 +0100

    chore: movers page UI fixed

--- components/landing/custom/AdServicesCard.tsx
+++ components/landing/custom/AdServicesCard.tsx
@@ -6,11 +6,11 @@ interface AdServicesCardProps {
 export const AdServicesCard: React.FC<AdServicesCardProps> = ({Icon, text}) => {
 
     return (
-        <div className="flex gap-4 bg-[#F3F7FF] p-0 shadow shadow-[0px 1.13187px 4.52747px 0px rgba(0, 0, 0, 0.15)] rounded-md">
+        <div className="flex gap-2 bg-[#F3F7FF] p-4 shadow shadow-[0px 1.13187px 4.52747px 0px rgba(0, 0, 0, 0.15)] rounded-md">
             <div className="icon bg-white p-2">
                 {Icon}
             </div>
-            <p className="text-[#2E5F9E] text-base md:text-base font-bold p-2">{text}</p>
+            <p className="text-[#2E5F9E] text-[13px] font-bold p-2">{text}</p>
         </div>
     )
 }
