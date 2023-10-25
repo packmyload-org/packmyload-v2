c54156cdaf426d2286286b187b1efc9ff025309b
Fiki-josh <fikioluwamayomikun@gmail.com>
2023-10-25 12:00:09 +0100

    chore: about us page fix

--- app/(default)/about-us/page.tsx
+++ app/(default)/about-us/page.tsx
@@ -6,17 +6,17 @@ import {AboutCard} from '@/components/AboutCard';
 
 export default function Home() {
   return (
-    <div className="flex justify-left bg-gray-100 mt-1">
+    <div className="flex justify-left bg-gray-100">
       <div className="w-full text-black" style={{ marginTop: '70px' }}>
-        <section className="gradient-bg grid items-center py-[20px] h-[fit-content]  ">
+        <section className="gradient-bg grid py-[20px] h-[fit-content]  ">
           <div className="cirle-bg">
             <div className="bg object-cover"></div>
           </div>
-          <div className="container py-12">
+          <div className="container py-12 max-w-6xl">
             <div className="join-wrap fl-wrap text-white">
-              <Row className="w-full flex" justify="space-between" align="middle">
+              <Row className="w-full flex justify-start">
                 <Col span={24} md={10} className="flex flex-col space-y-6 ">
-                  <h3 className="text-4xl font-bold">Learn more about Packmyload </h3>
+                  <h3 className="text-2xl md:text-4xl font-bold">Learn more about Packmyload </h3>
                   <p>
                     Check out our history, newsroom, and claims information, as well as opportunities
                     to become an agent, driver, or employee
@@ -37,7 +37,6 @@ export default function Home() {
                     </div>
                   </div>
                 </Col>
-                <Col span={24} md={10} className="flex items-center text-start md:text-end justify-center min-h-[160px]"></Col>
               </Row>
             </div>
           </div>
