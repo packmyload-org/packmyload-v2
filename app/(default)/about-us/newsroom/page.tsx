40dddd8c2613083196188222f0dc16a42cd47f3d
Fiki-josh <fikioluwamayomikun@gmail.com>
2023-10-25 14:10:27 +0100

    chore: fixes on about pages

--- app/(default)/about-us/newsroom/page.tsx
+++ app/(default)/about-us/newsroom/page.tsx
@@ -4,13 +4,13 @@ import React from "react";
 
 export default function Newsroom() {
   return (
-    <div className="flex justify-left min-h-screen bg-gray-100 mt-1">
-      <div className="w-full text-black" style={{ marginTop: "24px" }}>
-        <section className="gradient-bg grid items-center py-[20px] min-h-[250px] ">
+    <div className="flex justify-left min-h-screen bg-gray-100">
+      <div className="w-full text-black" style={{ marginTop: "70px" }}>
+        <section className="gradient-bg items-center py-[20px] h-[fit-content] ">
           <div className="cirle-bg">
             <div className="bg object-cover"></div>
           </div>
-          <div className="container py-12">
+          <div className="container py-12 max-w-6xl">
             <div className="join-wrap fl-wrap text-white">
               <Row
                 className="w-full flex"
@@ -18,7 +18,7 @@ export default function Newsroom() {
                 align="middle"
               >
                 <Col span={24} md={10} className="flex flex-col space-y-6">
-                  <h3 className="text-4xl font-bold">
+                  <h3 className="text-2xl md:text-4xl font-bold">
                     About Packmyload
                   </h3>
                   <p >
