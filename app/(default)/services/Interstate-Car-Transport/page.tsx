08980fd25ef1345db81654dc849a14fb8731b226
Fiki-josh <fikioluwamayomikun@gmail.com>
2023-10-28 18:37:03 +0100

    chore: Interstate-Car-Transport page fixes

--- app/(default)/services/Interstate-Car-Transport/page.tsx
+++ app/(default)/services/Interstate-Car-Transport/page.tsx
@@ -8,7 +8,7 @@ import {
     LightningSlash,
     SneakerMove,
   } from "@phosphor-icons/react";
-  
+import Link from 'next/link'
 
 export default function StudentMoves(){
 
@@ -19,12 +19,10 @@ export default function StudentMoves(){
                 firstText='Interstate Car Transport'
                 secondText='Interstate Car Transport'
             />
-            <br />
 
-
-            <div className="w-full h-auto py-20 mt-2">
-          <div className="relative bottom-10 flex flex-col gap-4 bg-[#EDF2F8] py-2">
-            <div className="flex flex-col gap-5 md:flex-row justify-between max-w-6xl mx-auto">
+            <div className="w-full h-auto mt-6">
+          <div className="flex flex-col gap-4 bg-[#EDF2F8] py-2">
+            <div className="flex flex-col gap-5 md:flex-row justify-between max-w-6xl mx-auto md:px-1">
               <div className="mx-auto w-full h-full flex items-center">
                 <img
                   src="/images/partner/packmyload_1695798593791.webp"
@@ -34,12 +32,12 @@ export default function StudentMoves(){
                 />
               </div>
 
-              <div className=" w-[90%] flex flex-col justify-center ">
-                <h2 className="text-2xl font-semibold w-full py-2 md:w-8/12  mt-4 px-2 lg:px-0 mx-auto text-[#566985]">
+              <div className=" w-[90%] flex flex-col justify-center">
+                <h2 className="text-xl lg:text-2xl font-semibold w-full py-2 mt-4 md:mt-0 px-2 lg:px-0 mx-auto text-[#566985]">
                   {" "}
                   Trusted Long Distance Moving Company
                 </h2>
-                <p className=" w-full text-sm  md:w-8/12  mt-4 px-2 lg:px-0 mx-auto text-[#878C9F]">
+                <p className=" w-full text-sm mt-4 px-2 lg:px-0 mx-auto text-[#878C9F]">
                   Packmyload has been providing customers with reliable and
                   professional long distance moving services for more than 5
                   years. For close to a decade of perfecting the interstate
@@ -56,72 +54,68 @@ export default function StudentMoves(){
               </div>
             </div>
 
-            <div className="flex flex-col gap-5 py-4 md:flex-row justify-between max-w-6xl mx-auto">
-              <div className=" w-[90%] flex flex-col justify-center ">
-                <h2 className="text-2xl font-semibold w-full  md:w-8/12  mt-4 px-2 lg:px-0 mx-auto text-[#566985]">
+            <div className="flex flex-col gap-5 py-4 md:flex-row justify-between max-w-6xl mx-auto md:px-1">
+              <div className=" w-[90%] flex flex-col justify-center">
+                <h2 className="text-xl lg:text-2xl font-semibold w-full mt-4 md:mt-0 px-2 lg:px-0 mx-auto text-[#566985]">
                   {" "}
                   Interstate Moving Services
                 </h2>
-                <p className=" w-full text-sm md:w-8/12  mt-4 px-2 lg:px-0 mx-auto text-[#878C9F]">
+                <p className=" w-full text-sm mt-4 px-2 lg:px-0 mx-auto text-[#878C9F]">
                   Whether you are moving just across state lines or across the
                   country, our interstate movers can help transport your
                   belongings to your new home with ease and simplicity!
                 </p>
 
-                <div>
-                  <div className="">
-                    <div className="grid grid-rows-2  md:grid-cols-2 lg:grid-cols-3 gap-3 py-6 ">
+                <div className="flex flex-wrap gap-3 py-6 ">
 
-                    <section className='flex gap-2 items-start mt-4'>
-                        <Image 
-                            src="/images/icons/aid-support.webp"
-                            alt='aid-support'
-                            width={100}
-                            height={100}
-                            className="object-contain " 
-                        />
-                        <div>
-                            <h1 className='text-lg text-[#4DB7FE] font-bold'>Packing and Unpacking </h1>
-                           
-                        </div>
-                    </section>
-                    <section className='flex gap-2 items-start mt-12'>
-                        <Image 
-                            src="/images/icons/guarantee-icon.webp"
-                            alt='gurantee-icon'
-                            width={100}
-                            height={100}
-                            className="object-contain " 
-                        />
-                        <div>
-                            <h1 className='text-lg text-[#4DB7FE] font-bold'>Storage Services </h1>
-                          
-                        </div>
-                    </section>
-                      {/* <AdServicesCard
-                        Icon={<Coin size={24} color="#2E5F9E" weight="fill" />}
-                        text="Earn a fixed salary "
+                  <section className='flex gap-1 items-center'>
+                      <Image 
+                          src="/images/icons/aid-support.webp"
+                          alt='aid-support'
+                          width={80}
+                          height={80}
+                          className="object-contain " 
                       />
-
-                      <AdServicesCard
-                        Icon={
-                          <CreditCard size={24} color="#2E5F9E" weight="fill" />
-                        }
-                        text="Get paid after every job "
+                      <div>
+                          <h1 className='text-base text-[#4DB7FE] font-bold'>Packing and Unpacking </h1>
+                        
+                      </div>
+                  </section>
+                  <section className='flex gap-1 items-center'>
+                      <Image 
+                          src="/images/icons/guarantee-icon.webp"
+                          alt='gurantee-icon'
+                          width={80}
+                          height={80}
+                          className="object-contain " 
                       />
+                      <div>
+                          <h1 className='text-base text-[#4DB7FE] font-bold'>Storage Services </h1>
+                        
+                      </div>
+                  </section>
+                    {/* <AdServicesCard
+                      Icon={<Coin size={24} color="#2E5F9E" weight="fill" />}
+                      text="Earn a fixed salary "
+                    />
 
-                      <AdServicesCard
-                        Icon={
-                          <SneakerMove
-                            size={24}
-                            color="#2E5F9E"
-                            weight="fill"
-                          />
-                        }
-                        text="Choose when to stop working "
-                      /> */}
-                    </div>
-                  </div>
+                    <AdServicesCard
+                      Icon={
+                        <CreditCard size={24} color="#2E5F9E" weight="fill" />
+                      }
+                      text="Get paid after every job "
+                    />
+
+                    <AdServicesCard
+                      Icon={
+                        <SneakerMove
+                          size={24}
+                          color="#2E5F9E"
+                          weight="fill"
+                        />
+                      }
+                      text="Choose when to stop working "
+                    /> */}
                 </div>
               </div>
 
@@ -137,25 +131,25 @@ export default function StudentMoves(){
           </div>
         </div>
 
-        <div className="flex flex-row flex-wrap gap-2 justify-center max-w-6xl mx-auto ">
+        <div className="flex flex-row flex-wrap gap-2 justify-center max-w-6xl mx-auto mt-6 ">
           <img
             src="/images/partner/packmyload_1697583737398.webp"
             alt="image-1"
-            className="w-[355px] h-[350px] relative bottom-20 object-cover"
+            className="w-[355px] h-[350px] bottom-20 object-cover"
           />
           <img
             src="/images/about/packmyload_1696572694895.webp"
             alt="image-2"
-            className="w-[355px] h-[350px] relative bottom-20 object-cover"
+            className="w-[355px] h-[350px] bottom-20 object-cover"
           />
           <img
             src="/images/partner/packmyload_1697583683638.webp"
             alt="image-3"
-            className="w-[355px] h-[350px] relative bottom-20 object-cover"
+            className="w-[355px] h-[350px] bottom-20 object-cover"
           />
         </div>
 
-        <div className="w-full h-auto">
+        <div className="w-full h-auto mt-6">
           <div className="flex flex-col max-w-6xl mx-auto px-2 lg:px-0">
             <h2 className="text-lg md:text-4xl font-bold text-[#566985]">
               What is a Long Distance Move?
@@ -192,7 +186,7 @@ export default function StudentMoves(){
 
 
             <section className="md:flex justify-center gap-4 max-w-6xl mx-auto mt-3 sm:block">
-                <div className='bg-white md:w-[70%] sm:w-[90%] p-6 rounded-md border-2 border-[#eee]'>
+                <div className='bg-white md:w-[50%] lg:w-[70%] sm:w-[90%] p-6 rounded-md border-2 border-[#eee]'>
                     <section>
                         <h1 className='text-xl font-bold text-[#566985]'>Small Moves</h1>
                         <p className='mt-2 text-sm text-[#878C9F]'>
@@ -224,7 +218,7 @@ export default function StudentMoves(){
                         </p>
                     </section>
                 </div>
-                <div className='md:w-[30%] sm:w-[90%]'> 
+                <div className='md:w-[50%] md:w-[50%] sm:w-[90%]'> 
                     <section className='flex gap-2 items-start mt-4'>
                         <Image 
                             src="/images/icons/aid-support.webp"
@@ -253,16 +247,16 @@ export default function StudentMoves(){
                     </section>
                 </div>
             </section>
-            <section>
-                <div className='flex flex-col mx-auto items-center justify-end h-full bg-[#4DB7FE]  py-3 my-7 ' >
-                  <h1 className='text-2xl font-bold text-[#ffff] pt-5 '>Request A Interstate Moving Quote</h1>
-
-                  <button  className="text-white shadow-lg border-2 text-xl font-bold p-2 w-[200px] bg-[#4BCBFE] outline-none transition-colors duration-600 hover:animate-pulse hover:bg-white hover:text-black my-5">Get Quote</button>
+            <section  className='mt-6'>
+                <div className='flex flex-wrap mx-auto items-center justify-center h-full bg-[#4DB7FE] gap-4 py-10 px-1' >
+                  <h1 className='text-xl md:text-2xl font-bold text-[#ffff] '>Request A Interstate Moving Quote</h1>
+                  
+                  <Link href='/move'>  
+                    <button  className="text-white shadow-lg border-2 text-xl font-bold p-2 w-[200px] bg-[#4BCBFE] outline-none transition-colors duration-600 hover:animate-pulse hover:bg-white hover:text-black">Get Quote</button>
+                  </Link>
 
                 </div>
             </section>
-
-            <br />
         </main>
     )
 }
