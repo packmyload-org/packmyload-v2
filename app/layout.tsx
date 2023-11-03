ea1f8603f26dc7393687824eed1542c4e6de26e3
m@nji <spontaneousmanji@gmail.com>
2023-11-03 14:02:34 +0100

    minor meta updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,11 +28,22 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         />
         {/* <!-- End Google Tag Manager --> */}
 
-        <meta name="description" content="packmyload.com is the preferred and rated number one packing and moving company in Lagos and Abuja, Nigeria. We offer international and interstate packing and moving to Lagos. Our staff help you with a swift, fast &amp; efficient move."></meta>
-            <meta property="og:image" content="https://www.packmyload.com/images/iconpackmyload.png"/>
+        <meta name="description" content="packmyload is the preferred and rated Nigeria # 1 packing and moving company in Lagos and Abuja. We offer international and interstate packing and moving to Lagos. Our staff help you with a swift, fast &amp; efficient move."></meta>
+            <meta property="og:image" content="https://www.packmyload.com/images/iconpackmyload.webp"/>
             <meta property="og:image:type" content="image/png"></meta>
             <meta property="og:image:height" content="342"></meta>
             <meta property="og:title" content="packmyload.com"/>
+            <meta property="og:image:width" content="342" />
+        {/* Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.packmyload.com" />
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@packmyload" />
+        {/* WhatsApp */}
+        <meta property="og:site_name" content="packmyload.com" />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:see_also" content="https://www.packmyload.com" />
             <link rel="icon" type='image/png' href="/favicon.ico" />
           <title>Packmyload</title>
       </head>
