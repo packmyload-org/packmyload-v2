efd6d05b08d0492df8239535b52f1c0fbbe9e07a
m@nji <spontaneousmanji@gmail.com>
2023-11-03 14:40:30 +0100

    chore: updating app

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         {/* <!-- End Google Tag Manager --> */}
 
         <meta name="description" content="packmyload is the preferred and rated Nigeria # 1 packing and moving company in Lagos and Abuja. We offer international and interstate packing and moving to Lagos. Our staff help you with a swift, fast &amp; efficient move."></meta>
+        <meta name="keywords" content="moving company, Nigeria, Lagos, Abuja, international relocation, interstate moving, professional movers" />
             <meta property="og:image" content="https://www.packmyload.com/images/iconpackmyload.webp"/>
             <meta property="og:image:type" content="image/png"></meta>
             <meta property="og:image:height" content="342"></meta>
