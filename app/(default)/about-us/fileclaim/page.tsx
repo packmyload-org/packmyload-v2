034cddb00d30f366d5ded93e97c0b51f196ae537
Fiki-josh <fikioluwamayomikun@gmail.com>
2023-11-04 01:51:52 +0100

    chore: file a claim page fixes

--- app/(default)/about-us/fileclaim/page.tsx
+++ app/(default)/about-us/fileclaim/page.tsx
@@ -4,13 +4,13 @@ import React from "react";
 
 export default function Leaders() {
   return (
-    <div className="flex justify-left min-h-screen bg-gray-100 mt-1">
-      <div className="w-full text-black" style={{ marginTop: "24px" }}>
-        <section className="gradient-bg grid items-center py-[5px] min-h-[150px] ">
+    <div className="flex justify-left min-h-screen bg-gray-100">
+      <div className="w-full text-black" style={{ marginTop: "70px" }}>
+        <section className="gradient-bg grid items-center py-[20px] lg:py-[5px]h-[fit-content] ">
           <div className="cirle-bg">
             <div className="bg object-cover"></div>
           </div>
-          <div className="container py-12">
+          <div className="container py-12 max-w-6xl">
             <div className="join-wrap fl-wrap text-white">
               <Row
                 className="w-full flex"
@@ -18,23 +18,18 @@ export default function Leaders() {
                 align="middle"
               >
                 <Col span={24} md={10} className="flex flex-col space-y-6">
-                  <h3 className="text-4xl font-bold">File A Moving Claim</h3>
+                  <h3 className="text-2xl md:text-4xl font-bold">File A Moving Claim</h3>
                   <p>
                     We are here to help! Let us help you with your claim, from
                     start to finish.
                   </p>
                 </Col>
-                <Col
-                  span={24}
-                  md={10}
-                  className="flex items-center text-start md:text-end justify-center min-h-[160px]"
-                ></Col>
               </Row>
             </div>
           </div>
         </section>
         <div className="w-full h-auto ">
-          <div className="grid grid-col px-10 py-7 ">
+          <div className="grid grid-col px-4 md:px-0 py-7 max-w-6xl mx-auto">
             <div>
               <h1 className="text-2xl font-bold text-blue-300 py-6">
                 For a recently completed household move with Packmyload:
