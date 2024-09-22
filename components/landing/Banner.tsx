34cf0d0234edf5e97876d140a9070b12a03f8483
Samagent97 <sammccarthy090@gmail.com>
2024-09-22 08:11:03 -0400

    reload

--- components/landing/Banner.tsx
+++ components/landing/Banner.tsx
@@ -5,7 +5,7 @@ export const Banner: React.FC = () => {
         <div className="bg-[#2E5F9E] text-center py-4 lg:px-4">
             <div className="p-2 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex" role="alert">
                 <span className="font-bold text-xs md:text-lg mr-2 text-center flex-auto">
-                    Discover more than just moving services <br />
+                    Discover more than just moving services. <br />
                     <span className="mt-3">Enjoy a moving experience that prioritizes safety and security.</span>
                 </span>
             </div>
